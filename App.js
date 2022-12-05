2629437a01b6f8da1e3adddbfd013d3c18e5b5ae
Chris Habgood <chabgood@gmail.com>
2022-12-04 22:18:43 -0600

    added 3 games, reset each games and balls

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, CSSProperties } from 'react';
+import { Text, View, TextInput, StyleSheet } from 'react-native';
+import { SelectList } from 'react-native-dropdown-select-list'
 
 export default function App() {
+  const [selected, setSelected] = React.useState("");
+  const [num_of_balls, setNumOfBalls] = React.useState(0);
+  const [num_of_games, setNumOfGames] = React.useState(0);
+  const data = [
+    { value: '8-ball', label: '8-ball' },
+    { value: 'Straight-Pool', label: 'Straight Pool' },
+    { value: 'x-balls', label: 'x-balls' }
+  ]
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View>
+        <Text style={styles.baseText}>Ghost Tracker App</Text>
+        <Text>Choose which game:</Text>
+        <SelectList
+          setSelected={(val) => { setSelected(val); setNumOfBalls(0); setNumOfGames(0)} }
+          data={data}
+          save="value"
+      />
+      { selected === '8-ball' &&
+        <TextInput
+          style={{height: 10, padding: 10}}
+          placeholder="How many games?"
+          onChangeText={text => setNumOfGames(text)}
+          defaultValue={num_of_games}
+        />  }
+      { selected !== "" && selected !== '8-ball' &&
+        <TextInput
+          style={{height: 10, padding: 10}}
+          placeholder="How many balls?"
+          onChangeText={text => setNumOfBalls(text)}
+          defaultValue={num_of_balls}
+        /> }
+      </View>
+
+      <View>
+        {selected !== "" &&
+          <>
+            <Text>Game: {selected}</Text>
+            { selected !== '8-ball' &&
+              <Text>Number of balls: {num_of_balls}</Text>
+        }
+            { selected === '8-ball' &&
+              <Text>Number of games: {num_of_games}</Text>
+            }
+        </>}
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  baseText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    paddingVertical: 100,
+  }
 });
+
